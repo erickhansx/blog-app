5af32bfc816401b5b8468d5e369d1e919775f7d3
erickhansx <erickhansx@gmail.com>
2023-01-27 16:30:56 -0600

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :posts, class_name: "Post", foreign_key: "post_id"
-  belongs_to :user, class_name: "User", foreign_key: "author_id"
+  belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   def comment_count
     posts.increment(:comments_counter)
   end
