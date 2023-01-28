32e6a0fa50a7cbdf12022fe1e12d8da38eacfd89
erickhansx <erickhansx@gmail.com>
2023-01-28 00:04:58 -0600

    Modify class methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   def comment_count
-    posts.increment(:comments_counter)
+    posts.increment!(:comments_counter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,6 @@ class Like < ApplicationRecord
   belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   def like_count
-    posts.increment(:likes_counter)
+    posts.increment!(:likes_counter)
   end
 end
