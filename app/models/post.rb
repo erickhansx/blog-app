24ceb805443c2a634f13fba306cf5efe56b5b558
erickhansx <erickhansx@gmail.com>
2023-02-03 09:21:22 -0600

    Update Post class.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
   validates :user, presence: true
   validates :title, presence: true
   validates :text, presence: true
