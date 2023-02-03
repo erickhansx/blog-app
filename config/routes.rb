b745876f6cddd3cbae5077f30f9f60877ce069f7
erickhansx <erickhansx@gmail.com>
2023-02-03 09:23:37 -0600

    Edit routes file to  add likes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create], path: 'posts'
+    resources :posts, only: [:index, :show, :new, :create], path: 'posts' do
+      resources :likes, only: [:create, :destroy]
+    end
   end
 
 end
