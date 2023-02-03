b783213145384938f20cd77bacf7f4d1db9031c0
erickhansx <erickhansx@gmail.com>
2023-02-03 09:21:36 -0600

    Update class User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy
   validates :name, presence: true
   validates :posts_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
