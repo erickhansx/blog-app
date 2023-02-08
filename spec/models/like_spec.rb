e78b116dbcc5a082e11d457a154ac0cc10e14b07
erickhansx <erickhansx@gmail.com>
2023-02-08 10:19:58 -0600

    Edit tests.

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Like, type: :model do
   subject { Like.new(post_id: test_post.id, author_id: test_user.id) }
 
   it 'increments the likes_counter on the associated post by 1' do
+    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    test_post = Post.create(author_id: test_user.id, title: 'Hello', text: 'This is my first post')
+    subject { Like.new(post_id: test_post.id, author_id: test_user.id) }
     subject.like_count
     expect(test_post.reload.likes_counter).to eql 1
   end
