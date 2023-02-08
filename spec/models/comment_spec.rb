2ea96de0848bbb17d332e1acf7be59b201c888b1
erickhansx <erickhansx@gmail.com>
2023-02-08 10:19:43 -0600

    Edit tests.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Comment, type: :model do
   subject { Comment.create(post_id: test_post.id, author_id: test_user, text: 'Hi Tom!') }
 
   it 'increments the likes_counter on the associated post by 1' do
+    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    test_post = Post.create(author_id: test_user.id, title: 'Hello', text: 'This is my first post')
+    subject { Comment.create(post_id: test_post.id, author_id: test_user, text: 'Hi Tom!') }
     subject.comment_count
     expect(test_post.reload.comments_counter).to eql 1
   end
