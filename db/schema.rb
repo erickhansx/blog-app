843147d079cdf8663e0976e3cab530ce8527ff07
erickhansx <erickhansx@gmail.com>
2023-02-10 11:08:34 -0600

    Update config for mail confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -53,13 +53,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_163834) do
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
