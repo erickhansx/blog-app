8184b1715d65ccc95abb4f1298d15911220891e9
erickhansx <erickhansx@gmail.com>
2023-02-09 18:50:15 -0600

    Update migration file to avoid errors.

--- db/migrate/20230209163834_add_devise_to_users.rb
+++ db/migrate/20230209163834_add_devise_to_users.rb
@@ -26,9 +26,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
 
       ## Confirmable
       t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
